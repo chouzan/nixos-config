adb6cf1a695b20ccff213c27d96c4d23c486c96d
Muhammad H. Fauzan <14904191+chouzan@users.noreply.github.com>
2026-01-13 22:55:58 +0700

    Update hosts/default

---
+++ hosts/default/storage.nix
@@ -0,0 +1,71 @@
+{ ... }:
+
+{
+  fileSystems = {
+    "/boot" = {
+      device = "/dev/disk/by-partlabel/boot";
+      fsType = "ext4";
+
+      options = [
+        "defaults"
+        "noatime"
+        "errors=remount-ro"
+        "nodev"
+        "nosuid"
+        "noexec"
+      ];
+    };
+
+    "/boot/efi" = {
+      device = "/dev/disk/by-partlabel/uefi";
+      fsType = "vfat";
+
+      options = [
+        "defaults"
+        "fmask=0077"
+        "dmask=0077"
+        "noatime"
+        "nodev"
+        "nosuid"
+        "noexec"
+      ];
+    };
+
+    "/" = {
+      device = "/dev/disk/by-partlabel/root";
+      fsType = "btrfs";
+
+      options = [
+        "subvol=@"
+        "defaults"
+        "noatime"
+        "compress=zstd:3"
+        "discard=async"
+        "space_cache=v2"
+      ];
+    };
+
+    "/home" = {
+      device = "/dev/disk/by-partlabel/home";
+      fsType = "btrfs";
+
+      options = [
+        "subvol=@home"
+        "defaults"
+        "noatime"
+        "compress=zstd:3"
+        "discard=async"
+        "space_cache=v2"
+      ];
+    };
+  };
+
+  swapDevices = [ { device = "/dev/disk/by-partlabel/swap"; } ];
+
+  boot = {
+    # Set resumeDevice if swap partlabel is not swap
+    # resumeDevice = "/dev/disk/by-partlabel/<swap-partlabel>";
+
+    tmp.useTmpfs = true;
+  };
+}
