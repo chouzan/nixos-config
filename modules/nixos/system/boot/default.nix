4c634984f21509f7d015269a094ae323931a012b
Muhammad H. Fauzan <14904191+chouzan@users.noreply.github.com>
2026-01-24 00:52:25 +0700

    Configure silent boot

--- modules/nixos/system/boot/default.nix
+++ modules/nixos/system/boot/default.nix
@@ -18,5 +18,25 @@
       timeout = 15;
       efi.efiSysMountPoint = lib.mkIf (config.fileSystems ? "/boot/efi") "/boot/efi";
     };
+
+    # Quiet boot
+
+    consoleLogLevel = 3;
+    initrd.verbose = false;
+
+    kernelParams = [
+      "quiet"
+      "splash"
+      "loglevel=3"
+      "rd.systemd.show_status=auto"
+      "rd.udev.log_level=3"
+      "systemd.show_status=auto"
+      "udev.log_level=3"
+      "vt.global_cursor_default=0"
+    ];
+
+    # Splash screen
+
+    plymouth.enable = true;
   };
 }
