c8f95641f518a0e9953cd5a0a5c3fc93e4da2e1f
Muhammad H. Fauzan <14904191+chouzan@users.noreply.github.com>
2026-02-02 16:20:39 +0700

    Fix priority hierarchy for mkModuleDefault, mkProfileDefault
    Add mkHostDefault. Priorities now override mkDefault (1000) but remain
    below defaultOverridePriority (100), so normal assignment still wins.

--- hosts/shared/system/locale.nix
+++ hosts/shared/system/locale.nix
@@ -1,19 +1,22 @@
-{ lib, ... }:
+{ libs, ... }:
 
+let
+  inherit (libs) utils;
+in
 {
   time = {
-    timeZone = lib.mkDefault "Asia/Jakarta";
+    timeZone = utils.mkHostDefault "Asia/Jakarta";
 
     # Make Linux use local time like Windows (dual-boot compatible)
-    hardwareClockInLocalTime = lib.mkDefault true;
+    hardwareClockInLocalTime = utils.mkHostDefault true;
   };
 
   # Set localisation
-  i18n.defaultLocale = lib.mkDefault "en_GB.UTF-8";
+  i18n.defaultLocale = utils.mkHostDefault "en_GB.UTF-8";
 
   # Set keyboard layout
   services.xserver.xkb = {
-    layout = lib.mkDefault "us";
-    variant = lib.mkDefault "";
+    layout = utils.mkHostDefault "us";
+    variant = utils.mkHostDefault "";
   };
 }
--- lib/utils.nix
+++ lib/utils.nix
@@ -3,27 +3,35 @@
 let
   getEnabledMonitors = monitors: lib.filter (m: !m.disabled) monitors;
 
-  # Returns value unless it's exactly false (allows null and other falsy values)
-  orUnless = fallback: value: if builtins.isBool value && !value then fallback else value;
   orIfNull = fallback: value: if value != null then value else fallback;
   orIfEmpty = fallback: value: if value != [ ] then value else fallback;
 
+  # Returns value unless it's exactly false (allows null and other falsy values)
+  orUnless = fallback: value: if builtins.isBool value && !value then fallback else value;
+
   # Priority hierarchy:
-  # - mkOptionDefault(1500)
-  # - * mkModuleDefault(1450)
-  # - * mkProfileDefault(1350)
-  # - mkDefault(1000)
-  # - mkForce(50)
-  mkModuleDefault = lib.mkOverride 1450;
-  mkProfileDefault = lib.mkOverride 1350;
+  # - mkOptionDefault (1500)
+  # - mkDefault (1000)
+  # - * mkModuleDefault (950)
+  # - * mkProfileDefault (850)
+  # - * mkHostDefault (750)
+  # - defaultOverridePriority (100)
+  # - mkImageMediaOverride (60)
+  # - mkForce (50)
+  # - mkVMOverride (10)
+  # Reference: https://github.com/NixOS/nixpkgs/blob/master/lib/modules.nix#L1488
+  mkModuleDefault = lib.mkOverride 950;
+  mkProfileDefault = lib.mkOverride 850;
+  mkHostDefault = lib.mkOverride 750;
 in
 {
   inherit
     getEnabledMonitors
+    mkHostDefault
     mkModuleDefault
     mkProfileDefault
-    orUnless
-    orIfNull
     orIfEmpty
+    orIfNull
+    orUnless
     ;
 }
