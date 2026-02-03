2e85c88cd737eb14de15399be88c7dd2a9a46362
Muhammad H. Fauzan <14904191+chouzan@users.noreply.github.com>
2026-02-03 22:09:06 +0700

    Use automatic-timezoned to set the timeZone

--- hosts/shared/system/locale.nix
+++ hosts/shared/system/locale.nix
@@ -4,19 +4,19 @@ let
   inherit (libs) utils;
 in
 {
-  time = {
-    timeZone = utils.mkHostDefault "Asia/Jakarta";
-
-    # Make Linux use local time like Windows (dual-boot compatible)
-    hardwareClockInLocalTime = utils.mkHostDefault true;
-  };
+  # Make Linux use local time like Windows (dual-boot compatible)
+  time.hardwareClockInLocalTime = utils.mkHostDefault true;
 
   # Set localisation
   i18n.defaultLocale = utils.mkHostDefault "en_GB.UTF-8";
 
-  # Set keyboard layout
-  services.xserver.xkb = {
-    layout = utils.mkHostDefault "us";
-    variant = utils.mkHostDefault "";
+  services = {
+    automatic-timezoned.enable = utils.mkHostDefault true;
+
+    # Set keyboard layout
+    xserver.xkb = {
+      layout = utils.mkHostDefault "us";
+      variant = utils.mkHostDefault "";
+    };
   };
 }
