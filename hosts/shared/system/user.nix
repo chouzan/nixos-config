e3ab351ad7c765ba4e3c156f948f7920f51ff05e
Muhammad H. Fauzan <14904191+chouzan@users.noreply.github.com>
2026-02-11 16:16:14 +0700

    Add uid option and make runtimeDirectory dynamic
    - Add modules.user.uid option (default 1000)
    - Set users.users uid from modules.user.uid
    - Use uid in runtimeDirectory instead of hardcoded value

--- hosts/shared/system/user.nix
+++ hosts/shared/system/user.nix
@@ -1,10 +1,12 @@
-{ machine, ... }:
+{ config, machine, ... }:
 
 let
   inherit (machine) user;
+  cfg = config.modules.user;
 in
 {
   users.users.${user.username} = {
+    inherit (cfg) uid;
     isNormalUser = true;
     description = user.name;
     extraGroups = [ "wheel" ];
--- modules/options/user.nix
+++ modules/options/user.nix
@@ -1,11 +1,25 @@
-{ lib, machine, ... }:
+{
+  config,
+  lib,
+  machine,
+  ...
+}:
 
 let
   inherit (lib) types;
   inherit (machine) user;
+
+  cfg = config.modules.user;
 in
 {
   options.modules.user = {
+    uid = lib.mkOption {
+      type = types.int;
+      default = 1000;
+      example = 1001;
+      description = "User's ID";
+    };
+
     homeDirectory = lib.mkOption {
       type = types.str;
       default = "/home/${user.username}";
@@ -15,7 +29,7 @@ in
 
     runtimeDirectory = lib.mkOption {
       type = types.str;
-      default = "/run/user/1000";
+      default = "/run/user/${toString cfg.uid}";
       example = "/run/user/1001";
       description = "XDG_RUNTIME_DIR path (system-managed, UID-based)";
     };
